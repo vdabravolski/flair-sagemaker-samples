0b84b8f496f07ddd582e24d165ad292f11774bda
Vadim Dabravolski <ec2-user@ip-172-16-56-199.us-east-2.compute.internal>
2020-06-02 00:15:04 +0000

    fix minor typos

--- processing_sources/processing.py
+++ processing_sources/processing.py
@@ -21,15 +21,16 @@ def main(args):
         for k, v in token_counts.items():
             token_counts[k] += file_string.count(k)
     
-    
-    with open(os.path.join(args.output_dir, 'token_counts.json'), 'w') as file:
+    output_file = 'token_counts.json'
+    with open(os.path.join(args.output_dir, output_file), 'w') as file:
         json.dump(token_counts, file)
+        
+    print(f"Processing completed. Results are stored in {output_file}.")
 
 
 if __name__ == "__main__":
     
     # Parse common arguments
-    print('Starting training...')
     parser = ArgumentParser()
     parser.add_argument('--tokens', type=str, required=True, help="specify which tokens to count in input files")
     parser.add_argument('--input-dir', type=str, default="/opt/ml/processing/input_data", help="local dir with files for processing")
